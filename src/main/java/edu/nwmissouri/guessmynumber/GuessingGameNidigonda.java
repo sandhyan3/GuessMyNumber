ee4d6ac6271e416c349c44cf54c125d1cccc52a7
sandhyan3 <S545239@NWMISSOURI.EDU>
2021-12-03 11:43:24 -0600

    modified message text

--- src/main/java/edu/nwmissouri/guessmynumber/GuessingGameNidigonda.java
+++ src/main/java/edu/nwmissouri/guessmynumber/GuessingGameNidigonda.java
@@ -26,12 +26,13 @@ public class GuessingGameNidigonda {
         while (userAnswer != secretNumber) {
             // TODO: create 3 local variables for the arguments so we know what they mean
             // showInputDialog - the first argument is always null (no parent)
-            // message = "Enter a guess between 1 and 100" // what type?
-            // title = "Guessing Game" // what type?
-            // messageType = JOptionPane.INFORMATION_MESSAGE
-            // Works the same as original 3 but more expressive - this is an example of a what? Hint: enum
+             
+            String message = "Enter a guess between 1 and 100";// what type?
+             String title = "Guessing Game"; // what type?
+             int messageType = JOptionPane.INFORMATION_MESSAGE;
+             //Works the same as original 3 but more expressive - this is an example of a what? Hint: enum
             String response = JOptionPane.showInputDialog(null,
-                    "Enter a guess between 1 and 100", "Guessing Game", 3);
+                    message, title, messageType);
             userAnswer = Integer.parseInt(response);
 
             JOptionPane.showMessageDialog(null, "" + calculateMessage(userAnswer, secretNumber, count));
@@ -44,14 +45,15 @@ public class GuessingGameNidigonda {
         // TODO: Instead of "Try Number" which sounds like we should try that as a guess...
         // Say "You've used " + count + guesses.
         // If count is one, use "guess", else use "guesses" HINT: Ternary operator
+        String s = "guesses";
         if (userAnswer <= 0 || userAnswer > 100) {
             return "Your guess is invalid";
         } else if (userAnswer == computerNumber) {
             return "Correct!\nTotal Guesses: " + count;
         } else if (userAnswer > computerNumber) {
-            return "Your guess is too high, try again.\nTry Number: " + count;
+            return "Your guess is too high, try again.\nyou've used: " + count + s;
         } else if (userAnswer < computerNumber) {
-            return "Your guess is too low, try again.\nTry Number: " + count;
+            return "Your guess is too low, try again.\nyou've used: " + count + s;
         } else {
             return "Your guess is incorrect\nTry Number: " + count;
         }
